c5e194b18fa174803f19dd7653dce31928238fcc
Humble <humbold4u@gmail.com>
2020-04-21 01:40:37 -0400

    If no overwrite, prompt if user still wants to create a new file

--- File_Creator.py
+++ File_Creator.py
@@ -26,5 +26,17 @@ def file_creator(file):
             with open(file, 'w+') as new_html:
                 new_html.seek(0)
                 new_html.write(html_format)
+        elif ow_YN == 'N':
+            print(f"Ok, {file} will not be overwritten.")
+            nf_YN = input("Do you still want to create a new file? Y or N: ")
+            nf_YN = nf_YN.upper()
+            if nf_YN == 'Y':
+                file_creator(input(f"choose a name for your file (remember you cannont use {file}): "))
+            elif nf_YN == 'N':
+                print('Ok have a blessed day! Oh, and let me know if you ever need an html file!')
+            else:
+                print("Sorry, I am not smart enough to understand your input yet. Soon I shall be though.")
+        else:
+            print("Sorry, I am not smart enough to understand your input yet. Soon I shall be though.")
 
 file_creator(file_name)    
